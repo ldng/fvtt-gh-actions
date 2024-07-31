c460bd24bc3796f630a41eddde5efac8e67d7dc6
Ludovic Danigo <ldng@free.fr>
2024-08-01 01:59:10 +0200

    Try to imort fs differently

--- pack.mjs
+++ pack.mjs
@@ -1,5 +1,5 @@
-import {fs} from 'fs';
-import {path} from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import { default as YAML } from "js-yaml";
 import { ClassicLevel } from "classic-level";
 
